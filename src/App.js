bf239c99c0f52129e46662f227d8275930428448
KnotEnvy <jwsnyder@gmail.com>
2023-07-24 13:26:02 -0400

    KnotzGPT React Express

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
+import './normal.css'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <aside className="sidemenu">
+        <h1>Pick Model</h1>
+        <div className="side-menu-button">
+          <span>+</span>
+          Get Started!
+        </div>
+
+      </aside>
+      <section className='chatbox'>
+        <div className="chat-log">
+          <div className="chat-message">
+            <div className="chat-message-center">
+                <div className="avatar">Knotz</div>
+                <div className="message">Genius Bot</div>
+            </div>
+          </div>
+        </div>
+        <div className="chat-input-holder">
+          <textarea className='chat-input-textarea' rows='1' placeholder='Type Your Prompt Here'></textarea>
+        </div>
+      </section>
     </div>
   );
 }
